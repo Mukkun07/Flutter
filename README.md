48692ca2e264f21fe3ee4aca451ab055aa6cc089
murakami.kenta <murakami.kenta01@gmail.com>
2021-05-11 15:48:25 +0900

    Todoアプリ作成途中

--- README.md
+++ README.md
@@ -1,28 +1,23 @@
-# Flutter
-FlutterでのAndroid・iOSアプリの開発
+# FlutterでのAndroid・iOSアプリの開発
 
 Dart言語を使用しAndroid・iOSアプリを開発した内容を載せています
 
-・Windowsでの環境構築
+## Windowsでの環境構築
   
-   1.flutterSDKのインストール
+   1. flutterSDKのインストール
 
-   2.パス(環境変数)の更新
+   2. パス(環境変数)の更新
 
-   3.AndroidStudioのインストール
+   3. AndroidStudioのインストール
 
-   4.AndroidStudioにflutterとDartのインストール
+   4. AndroidStudioにflutterとDartのインストール
 
-   5.エミュレーターのインストール
+   5. エミュレーターのインストール
 
-   6.新規プロジェクトの作成
+   6. 新規プロジェクトの作成
 
-   7.デモプログラムの起動
+   7. デモプログラムの起動
 
-
-
-
-
-
-参考
-Flutter公式 : https://flutter.dev/docs/get-started/install
+## 参考
+   - [Flutter公式サイト](https://flutter.dev/docs/get-started/install)
+   - [Flutterで始めるアプリ開発](https://www.flutter-study.dev/)
---
+++ todo_app/lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // 最初に表示するWidget
+  runApp(MyTodoApp());
+}
+
+class MyTodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // アプリ名
+      title: 'My Todo App',
+      theme: ThemeData(
+        // テーマカラー
+        primarySwatch: Colors.blue,
+      ),
+      // リスト一覧画面を表示
+      home: TodoListPage(),
+    );
+  }
+}
+
+// リスト一覧画面用のWidget
+class TodoListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // AppBarを表示し、タイトルを設定
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
+      // ListViewを使いリスト一覧を表示
+      body: ListView(
+        children: <Widget>[
+          // CardとListTileを使い、簡単に整ったUIを作成
+          Card(
+            child: ListTile(
+              title: Text('たなか'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('さとう'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('やまだ'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('かとう'),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // "push"で新規画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 遷移先の画面としてリスト追加画面を指定
+              return TodoAddPage();
+          }),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// リスト追加画面用のWidget
+class TodoAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("リスト追加"),
+      ),
+      body: Container(
+        // 余白を付ける
+        padding: EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // テキスト入力
+            TextField(),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // リスト追加ボタン
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('リスト追加', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // キャンセルボタン
+              child: TextButton(
+                // ボタンをクリックした時の処理
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  Navigator.of(context).pop();
+                },
+                child: Text('キャンセル'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
